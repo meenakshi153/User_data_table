046ba1aa3af40755d30cd09afca0c006f3be7ae8
meenakshi kathivaru <kathivarumeenakshi@gmail.com>
2025-10-17 17:04:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Todolist from "./components/Todolist";
-import RegisterForm from "./components/Registerform";
-import Todolistlearning from "./components/Todolistlearning";
-import Useataeanduseeffectandtostify from "./components/Useataeanduseeffectandtostify";
+
 import Onemoretimelearningtodolist from "./components/Onemoretimelearningtodolist";
-import Formnuchidata from './components/Formnuchidata';
-import Againnewlearning from './components/Againnewlearning';
-import Longtimetodolistlearning from './components/Longtimetodolistlearning';
-import Useeffectlearning from './components/Useeffectlearning';
+
 import Onemoretimelearnuseeffect from './components/Onemoretimelearnuseeffect';
-import Usereflearning from './components/Usereflearning';
+
 
 
 const App = () => {
